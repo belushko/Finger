d9407be3beec3890908d798ceb538936c7a5dddd
ebe <ebe@nemetos.net>
2018-03-28 21:17:46 +0300

    add Core and Test projects

---
+++ Finger.Core.Tests/MultiplierTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace Finger.Core.Tests
+{
+    [TestFixture]
+    public class MultiplierTest
+{
+        [Test]
+        public void Multiplier_Multiply_RightResult()
+        {
+            var x = 4;
+            var y = 6;
+            var expectedResult = 24;
+            var multiplier = new Multiplier();
+
+            var multiplicationКesult = multiplier.Muktiply(x, y);
+
+            Assert.AreEqual(multiplicationКesult, expectedResult);
+        }
+
+    [Test]
+    public void Multiplier_Multiply_RightResult2()
+    {
+        var x = 4;
+        var y = 0;
+        var expectedResult = 0;
+        var multiplier = new Multiplier();
+
+        var multiplicationКesult = multiplier.Muktiply(x, y);
+
+        Assert.AreEqual(multiplicationКesult, expectedResult);
+    }
+
+    [Test]
+    public void Multiplier_Multiply_RightResult3()
+    {
+        var x = -4;
+        var y = 5;
+        var expectedResult = -20;
+        var multiplier = new Multiplier();
+
+        var multiplicationКesult = multiplier.Muktiply(x, y);
+
+        Assert.AreEqual(multiplicationКesult, expectedResult);
+    }
+    }
+}
---
+++ Finger.Core/Multiplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finger.Core
+{
+    public class Multiplier
+    {
+        public double Muktiply(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
